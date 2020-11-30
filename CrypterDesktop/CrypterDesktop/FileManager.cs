9389f479ec94765277ff3d01f19cd78fa0a21b18
burachevsky <burachevsky666@gmail.com>
2020-11-30 14:55:27 +0300

    fix file opening bug

--- CrypterDesktop/CrypterDesktop/FileManager.cs
+++ CrypterDesktop/CrypterDesktop/FileManager.cs
@@ -21,13 +21,14 @@ namespace CrypterDesktop
 
         public void OpenReadFileDialog()
         {
-            App.Run(() =>
+            FileDialog(() =>
             {
                 var openFileDialog = new OpenFileDialog();
                 openFileDialog.CheckPathExists = true;
                 openFileDialog.CheckFileExists = true;
                 openFileDialog.Filter = "Text Files (*.txt)|*.txt|Document Files (*.docx)|*.docx";
                 openFileDialog.Multiselect = false;
+                openFileDialog.ShowReadOnly = true;
                 if (openFileDialog.ShowDialog() == true)
                 {
                     var text = ReadFile(openFileDialog.FileName);
@@ -40,13 +41,41 @@ namespace CrypterDesktop
 
         public void OpenSaveFileDialog(string text)
         {
-            App.Run(() =>
+            FileDialog(() =>
             {
                 var saveFileDialog = new SaveFileDialog();
                 saveFileDialog.Filter = "Text Files (*.txt)|*.txt|Document Files (*.docx)|*.docx";
                 if (saveFileDialog.ShowDialog() == true)
                 {
                     Save(saveFileDialog.FileName, text);
+                }
+            });
+        }
+
+        private void FileDialog(Action action)
+        {
+            App.Run(() =>
+            {
+                while (true)
+                {
+                    try
+                    {
+                        action?.Invoke();
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        var text =
+                            $"Не удалось открыть файл.\nВозможно, он занят другим процессом или поврежден.\nПричина ошибки: {e.GetType()}\nХотите попробовать заново?";
+                        var btn = MessageBoxButton.YesNo;
+                        var result = MessageBox.Show(text, "", btn);
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            continue;
+                        }
+
+                        break;
+                    }
                 }
             });
         }
@@ -96,7 +125,9 @@ namespace CrypterDesktop
 
             File.Delete("temp.docx");
 
-            return text.Substring(71);
+            var shift = text.EndsWith("\r\n") ? 2 : 0;
+
+            return text.Substring(71, text.Length - 71 - shift);
         }
 
         private void WriteDocs(string path, string text)
@@ -107,38 +138,32 @@ namespace CrypterDesktop
                 var paragraph = section.AddParagraph();
                 paragraph.AppendText(text);
 
-                try
+                doc.SaveToFile(path, FileFormat.Docx);
+
+                using (var stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
                 {
-                    doc.SaveToFile(path, FileFormat.Docx);
-                    using (var stream = new FileStream(path, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                    using (var zipArchive = new ZipArchive(stream, ZipArchiveMode.Update))
                     {
-                        using (var zipArchive = new ZipArchive(stream, ZipArchiveMode.Update))
+                        string resultDoc;
+
+                        using (var xmlDoc = zipArchive.GetEntry("word/document.xml").Open())
                         {
-                            string resultDoc;
-
-                            using (var xmlDoc = zipArchive.GetEntry("word/document.xml").Open())
+                            using (var streamReader = new StreamReader(xmlDoc))
                             {
-                                using (var streamReader = new StreamReader(xmlDoc))
-                                {
-                                    var document = streamReader.ReadToEnd();
-                                    resultDoc = document.Remove(document.IndexOf("<w:p>"), 192);
-                                }
+                                var document = streamReader.ReadToEnd();
+                                resultDoc = document.Remove(document.IndexOf("<w:p>"), 192);
                             }
+                        }
 
-                            using (var replacedFile = zipArchive.GetEntry("word/document.xml").Open())
+                        using (var replacedFile = zipArchive.GetEntry("word/document.xml").Open())
+                        {
+                            replacedFile.SetLength(resultDoc.Length);
+                            using (var writer = new StreamWriter(replacedFile))
                             {
-                                replacedFile.SetLength(resultDoc.Length); 
-                                using (var writer = new StreamWriter(replacedFile))
-                                {
-                                    writer.Write(resultDoc);
-                                }
+                                writer.Write(resultDoc);
                             }
                         }
                     }
-                }
-                catch (Exception e)
-                {
-                    MessageBox.Show($"{e.GetType()} {e.StackTrace}");
                 }
             }
         }
---
+++ CrypterDesktop/CrypterDesktopTests/FileManagerTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CrypterDesktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CrypterCore;
+
+namespace CrypterDesktopTests
+{
+    [TestClass]
+    public class FileManagerTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var cipher = new VigenereCipher("Скоприон", Alphabets.RUSSIAN);
+            var text = "Какой-то текст просто для теста";
+            var file = "tmp.docx";
+
+            var fileManager = new FileManager(null);
+
+            fileManager.Save(file, cipher.Encrypt(text));
+
+            var encryptedText = fileManager.ReadFile(file);
+            var decryptedEncryptedText = cipher.Decrypt(encryptedText);
+
+            Assert.IsTrue(text.Equals(decryptedEncryptedText));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var cipher = new VigenereCipher("Скоприон", Alphabets.RUSSIAN);
+            var text = "Какой-то текст просто для теста";
+            var file = "tmp.txt";
+
+            var fileManager = new FileManager(null);
+
+            fileManager.Save(file, cipher.Encrypt(text));
+
+            var encryptedText = fileManager.ReadFile(file);
+
+            Assert.IsTrue(text.Equals(cipher.Decrypt(encryptedText)));
+        }
+    }
+}
